c9ead1cec2129f8d62771014b5990de66ef3ffca
NadineAzebaze <nadine-carel.azebaze-zewo@etu.univ-cotedazur.fr>
2022-03-29 15:43:47 +0200

    Rendu TD6 final

--- TD6/MathLibrary/ClientSoap/Program.cs
+++ TD6/MathLibrary/ClientSoap/Program.cs
@@ -16,15 +16,19 @@ namespace serveurSoap
             int sum = mathsOperationsClient.add(3, 4);
             Console.WriteLine("sum 3+4 = ");
             Console.WriteLine( sum);
+
             int multiply = mathsOperationsClient.multiply(3, 4);
             Console.WriteLine( "multiply 3*4 = ");
             Console.WriteLine(multiply);
+
             int substract = mathsOperationsClient.substract(7, 4);
             Console.WriteLine("substract 7-4 = ");
             Console.WriteLine(substract);
+
             double divide = mathsOperationsClient.divide(7, 4);
             Console.WriteLine("divide 9by4 = ");
             Console.WriteLine(divide);
+
             Console.ReadLine();
         }
     }
